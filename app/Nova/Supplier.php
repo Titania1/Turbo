24185ff8bef6ba5c0ed4db2a3e3e5b5b439934cb
CaddyDz <caddydz4@gmail.com>
2020-07-28 15:13:33 +0100

    Apply CS fixes
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Nova/Supplier.php
+++ app/Nova/Supplier.php
@@ -7,8 +7,8 @@ namespace App\Nova;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Place;
+use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Supplier extends Resource
--- app/Nova/Templates/FooterOptions.php
+++ app/Nova/Templates/FooterOptions.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Nova\Templates;
 
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\Place;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Place;
 use Whitecube\NovaPage\Pages\Template;
 
 class FooterOptions extends Template
