53044f30972a2d62362ead0bcc08098b7cdc85a2
CaddyDz <caddydz4@gmail.com>
2020-08-17 17:32:17 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ app/Catalog/Part.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Catalog;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Part extends Model
+{
+	/**
+	 * The connection name for the model.
+	 *
+	 * @var string
+	 */
+	protected $connection = 'tecdoc';
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'articles';
+}
