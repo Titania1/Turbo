5ba197a7322a8dcd59aa6a0e709a7341a4e7722b
CaddyDz <caddydz4@gmail.com>
2020-08-19 19:18:38 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ database/seeds/ModelSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class ModelSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Storage::disk('public')->deleteDirectory('models');
+		Storage::disk('public')->makeDirectory('models');
+		require_once dirname(__FILE__) . '/data/brands.php';
+		foreach ($brands as $image_url => $name) {
+			// Use the name to grab the model from tecdoc using the brand name to get the id
+			$ManufacturerId = \DB::connection('tecdoc')->table('manufacturers')
+				->select('id')->where('Matchcode', $name)->limit(1)->pluck('id')->first();
+			// We select the id of the model to use it to grab the image from ghiar
+			$models = \DB::connection('tecdoc')->table('models')
+				->select('id', 'From', 'To', 'Description')
+				->where('ManufacturerId', $ManufacturerId)->whereDate('To', '>', '1980-01-01')->get();
+			// Use that id to grab the models from tecdoc
+			$response = Http::get('https://ghiar.com/' . $image_url);
+			Storage::disk('public')->put("/brands/$name.png", $response->body());
+			//
+		}
+	}
+}
