9d47bf3c3c34619a532b5a54f54603f9c75e679f
titania1 <widadwid.ww@gmail.com>
2020-08-23 22:47:27 +0100

    Brands tabs UI

--- resources/views/partials/brands.blade.php
+++ resources/views/partials/brands.blade.php
@@ -1,5 +1,6 @@
 <div class="block block-brands block-brands--layout--columns-8-full">
 	<div class="container">
+<<<<<<< Updated upstream
 		<ul class="block-brands__list">
 			@foreach ($brands as $brand)
 			<li class="block-brands__item">
@@ -11,5 +12,49 @@
 			<li class="block-brands__divider" role="presentation"></li>
 			@endforeach
 		</ul>
+=======
+		<div class="product__tabs product-tabs">
+			<ul class="product-tabs__list">
+				<li class="product-tabs__item product-tabs__item--active">
+					<a href="#passenger_brands">
+						Véhicules de Tourisme
+					</a>
+				</li>
+				<li class="product-tabs__item">
+					<a href="#commercial_brands">
+						Bus et Véhicules Utilitaires
+					</a>
+				</li>
+			</ul>
+			<div class="product-tabs__content">
+				<div class="product-tabs__pane product-tabs__pane--active" id="passenger_brands">
+					<ul class="block-brands__list">
+						@foreach($brands as $brand)
+						<li class="block-brands__item">
+							<a href="{{ route('brand', ['brand' => $brand]) }}" class="block-brands__item-link">
+								<img src="{{ secure_asset('storage/' . $brand->logo) }}" alt="@lang('Photo')" loading="lazy">
+								<span class="block-brands__item-name">{{ $brand->name }}</span>
+							</a>
+						</li>
+						<li class="block-brands__divider" role="presentation"></li>
+						@endforeach
+					</ul>
+				</div>
+				<div class="product-tabs__pane" id="commercial_brands">
+					<ul class="block-brands__list">
+						@foreach($commercial_brands as $brand)
+						<li class="block-brands__item">
+							<a href="{{ route('brand', ['brand' => $brand]) }}" class="block-brands__item-link">
+								<img src="{{ secure_asset('storage/' . $brand->logo) }}" alt="@lang('Photo')" loading="lazy">
+								<span class="block-brands__item-name">{{ $brand->name }}</span>
+							</a>
+						</li>
+						<li class="block-brands__divider" role="presentation"></li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</div>
+>>>>>>> Stashed changes
 	</div>
 </div>
