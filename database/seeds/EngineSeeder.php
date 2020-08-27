4350bc6159dce1dcebe9b4d8f00f0e3768ba922b
CaddyDz <caddydz4@gmail.com>
2020-08-27 16:32:25 +0100

    Engine entity skeleton
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ database/seeds/EngineSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EngineSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		//
+	}
+}
