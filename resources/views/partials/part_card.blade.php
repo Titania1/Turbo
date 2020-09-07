c10f98b8871b976dea6163d3697d9cd81744c4aa
titania1 <widadwid.ww@gmail.com>
2020-09-07 21:22:06 +0100

    Add to cart, make the input spinner work
    Signed-off-by: titania1 <widadwid.ww@gmail.com>

--- resources/views/partials/part_card.blade.php
+++ resources/views/partials/part_card.blade.php
@@ -51,10 +51,10 @@
 			<div class="rating__body">
 				@for($i = 0; $i < $part->rating; $i++)
 					<div class="rating__star rating__star--active"></div>
-				@endfor
-				@for($s = $part->rating; $s < 5; $s++)
-					<div class="rating__star"></div>
-				@endfor
+					@endfor
+					@for($s = $part->rating; $s < 5; $s++) <div class="rating__star">
+			</div>
+			@endfor
 		</div>
 	</div>
 	<div class="product-card__rating-label">3 on 7 reviews</div>
@@ -78,3 +78,4 @@
 		</button>
 	</form>
 </div>
+
