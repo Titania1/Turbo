ccf7180b06131bc48154191d634b132c5c0c3851
CaddyDz <caddydz4@gmail.com>
2020-09-15 03:10:15 +0100

    Offline manufacturers seeder
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ database/seeds/catalog/ManufacturerSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Brand;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+
+class ManufacturerSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Storage::disk('public')->deleteDirectory('brands');
+		Storage::disk('public')->makeDirectory('brands');
+		require_once dirname(__FILE__, 2) . '/data/brands.php';
+		foreach ($brands as $image_url => $name) {
+			// $response = Http::get('https://ghiar.com/' . $image_url);
+			// Storage::disk('public')->put("/brands/$name.png", $response->body());
+			Brand::create([
+				'name' => $name,
+				// 'logo' => "brands/$name.png",
+			]);
+			break;
+		}
+
+		// Seed commercial vehicles
+		foreach ($commercialVehicles as $image_url => $name) {
+			// $response = Http::get('https://ghiar.com/' . $image_url);
+			// Storage::disk('public')->put("/brands/$name.png", $response->body());
+			Brand::create([
+				'name' => $name,
+				// 'logo' => "brands/$name.png",
+				'is_commercial' => true,
+			]);
+		}
+	}
+}
